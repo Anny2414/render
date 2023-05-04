5352fd9e77de7ae4d6305a2d5ce67cff67dc9787
ingen1us <yeinernaranjo@gmail.com>
2023-05-04 07:16:44 -0500

    Se añadió rol y permisos

--- yourburger/models.py
+++ yourburger/models.py
@@ -1,9 +1,29 @@
 from django.db import models
 
 # Create your models here.
+class Role(models.Model):
+    name = models.CharField(max_length=50)
+    created_at = models.DateField(auto_now_add=True)
+    status = models.BooleanField(default=True)
+
+    def __str__(self):
+        return self.name
+
+class Permission(models.Model):
+    module_name = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.name
+
+class DetallePermiso(models.Model):
+    role = models.ForeignKey(Role, on_delete=models.CASCADE)
+    permission = models.ForeignKey(Permission, on_delete=models.CASCADE)
+
 class User(models.Model):
+    role = models.ForeignKey(Role, on_delete=models.CASCADE)
     username = models.CharField(max_length=50)
     email = models.EmailField(max_length=254)
+    password = models.CharField(max_length=50)
     document = models.CharField(max_length=10)
     name = models.CharField(max_length=50)
     lastname = models.CharField(max_length=50)
